6775845c1a6c90a454710a1c9a241ccece01e497
natanlimaz <natanlimadev@gmail.com>
2024-10-01 11:23:26 -0300

    feat: Criação do metadata estático e do metadata dinâmico da página

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,21 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "DevMotors - Sua oficina especializada!",
   description: "Oficina para contratação de serviços em veículos",
+  keywords: ["oficina", "oficina carros", "carros", "manutenção de carros"],
+  openGraph: {
+    title: "DevMotors - Sua oficina especializada!",
+    images: [`${process.env.NEXT_PUBLIC_URL}/logo.jpg`]
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true
+    }
+  }
 };
 
 export default function RootLayout({
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -5,6 +5,53 @@ import { PostProps } from "@/utils/post.type";
 import { Phone } from "lucide-react";
 import { Container } from "@/components/container";
 import Image from "next/image";
+import { Metadata } from "next";
+
+export async function generateMetadata({ params: { slug } }: {
+    params: { slug: string }
+}): Promise<Metadata> {
+
+    try {
+
+        const { objects }: PostProps = await getItemBySlug(slug)
+        .catch(() => {
+            return  {
+                title: "DevMotors - Sua oficina especializada!",
+                description: "Oficina para contratação de serviços em veículos",
+            }
+        });
+
+        return {
+            title: `DevMotors - ${objects[0].title}`,
+            description: `${objects[0].metadata.description.text}`,
+            openGraph: {
+                title: `DevMotors - ${objects[0].title}`,
+                images: [`${objects[0].metadata.banner.url}`]
+            },
+            robots: {
+                index: true,
+                follow: true,
+                nocache: true,
+                googleBot: {
+                    index: true,
+                    follow: true,
+                    noimageindex: true
+                }
+            }
+        }
+
+    }
+    catch(error) {
+        return  {
+            title: "DevMotors - Sua oficina especializada!",
+            description: "Oficina para contratação de serviços em veículos",
+        }
+    }
+
+    return {
+
+    }
+}
 
 export default async function Post({ params: { slug } }: {
     params: { slug: string }
